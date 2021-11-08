2fd2f53f5ac6cd51235459c7d5d6e634898c0ca1
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-08 13:36:28 +0100

    printf uploaded 1st task

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _printf - entry point
+ * @format:struct
+ * Return:Null or number of characters used
+ */
+int _printf(const char *format)
+{
+va_list apt;
+unsigned int i = 0, count = 0, T_func = 0;
+char *s;
+if (!format || format[0] == '%' && format[1] == '\0')
+return (-1);
+while (*(fromat + i) && format)
+{
+if (*(format + i) == '%')
+{
+T_func = get_func(*(format + (i + 1)), arg);
+if (T_func != 0)
+{
+count += T_func;
+i += 2;
+continue;
+}
+if (*(format + (i + 1)) == '\0')
+{
+_putchar(*(format + i));
+count++;
+}
+}
+i++;
+}
+va_end(arg);
+return (count);
+}
