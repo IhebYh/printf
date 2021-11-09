815e1988142c5258db2e75325b873525f653a3fb
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-09 13:57:59 +0100

    update

--- get_function.c
+++ get_function.c
@@ -9,7 +9,7 @@ int get_func(const char s, va_list ap)
 {
 int i = 0, count = 0;
 printf_t func_list[] = {{'c', print_c}, {'s', print_s}, {'%', print_percent},
-{'d', print_i}, {'i', print_i}};
+{'d', print_i}, {'i', print_i}, {'b', print_b}};
 while (func_list[i].c != 0)
 {
 if (func_list[i].c == s)
