f41cbc89f54c02b02a28edebaad18c8c88bb4aee
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-09 13:35:55 +0100

    Hot fix

--- get_function.c
+++ get_function.c
@@ -8,7 +8,8 @@
 int get_func(const char s, va_list ap)
 {
 int i = 0, count = 0;
-printf_t func_list[] = {{'c', print_c}, {'s', print_s}, {'%', print_percent}};
+printf_t func_list[] = {{'c', print_c}, {'s', print_s}, {'%', print_percent},
+{'d', print_i}, {'i', print_i}};
 while (func_list[i].c != 0)
 {
 if (func_list[i].c == s)
