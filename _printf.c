17764d653c536b43e044daa9760e95f23e8508f8
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-10 13:51:15 +0100

    betty style

--- _printf.c
+++ _printf.c
@@ -11,16 +11,16 @@ int i = 0, count = 0, func = 0;
 if (!format || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 va_start(apt, format);
-while (*(format + i) && format)
+while (format[i] && format)
 {
-if (*(format + i) != '%')
+if (format[i] != '%')
 {
-_putchar(*(format + i));
+_putchar(format[i++]);
 count++;
 }
-if (*(format + i) == '%')
+if (format[i] == '%')
 {
-if ((*(format + i + 1)) == '%')
+if ((format[i + 1]) == '%')
 {
 _putchar('%');
 count++;
@@ -29,21 +29,20 @@ continue;
 }
 else
 {
-func = get_func(*(format + (i + 1)), apt);
+func = get_func(format[i + 1]), apt);
 if (func != 0)
 {
 count += func;
 i += 2;
 continue;
 }
-if (*(format + (i + 1)) == '\0')
+if (format[i + 1] == '\0')
 {
-_putchar(*(format + i));
+_putchar(format[i++]);
 count++;
 }
 }
 }
-i++;
 }
 va_end(apt);
 return (count);
