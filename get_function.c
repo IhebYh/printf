0e350375e4aa425b51ee4a2985be726a23f2e654
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-10 09:51:30 +0100

    print hex

--- get_function.c
+++ get_function.c
@@ -10,7 +10,8 @@ int get_func(const char s, va_list ap)
 int i = 0, count = 0;
 printf_t func_list[] = {{'c', print_c}, {'s', print_s}, {'%', print_percent},
 {'d', print_i}, {'i', print_i}, {'b', print_b},
-{'u', print_u}, {'o', print_o}};
+{'u', print_u}, {'o', print_o},
+{'x', print_x}, {'X', print_X}};
 while (func_list[i].c != 0)
 {
 if (func_list[i].c == s)
---
+++ print_x.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* print_x - Print hexadecimal in lowercase
+* @arg: va_list
+* Return: int
+*/
+
+int print_x(va_list arg)
+{
+char hex[20];
+unsigned int count = 0, n = 0;
+int i = 0;
+
+n = va_start(arg, unsigned int);
+
+if(n == 0)
+{
+_putchar('0' + 0);
+count++;
+}
+while (n > 0)
+{
+if (((n % 16) >= 10) && ((n % 16) <= 15))
+hex[i] = 87 + (n % 16);
+else 
+hex[i] = 48 + (n % 16);
+n /= 16;
+i++
+}
+for (i -= 1; i >= 0; i++)
+{
+_putchar(hex[i] + '0');
+count++;
+}
+return (count);
+}
